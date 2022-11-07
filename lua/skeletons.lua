880022592fcd8e4f9ff596e349f394943ab14e39
theNEOkun <sven@raswill.se>
2022-11-07 13:35:19 +0100

    Updated how the skeletons are created

--- lua/skeletons.lua
+++ lua/skeletons.lua
@@ -3,44 +3,24 @@ local autogroup = require("utils").autogroup;
 
 local augroup = autogroup("skeletoncreation");
 
--- When createing a readme-file
-autocmd("BufNewFile",
+local list = {
 	{ "README.md", "readme.md" },
-	"0r ~/library/skeletons/README.md",
-	augroup)
-
--- When creating a bash-file
-autocmd("BufNewFile",
-	{ "*.sh" },
-	"0r ~/library/skeletons/bash.sh",
-	augroup)
-
--- When creating a makefile
-autocmd("BufNewFile",
 	{ "Makefile", "makefile" },
-	"0r ~/library/skeletons/Makefile",
-	augroup)
-
--- When creating a new c-main
-autocmd("BufNewFile",
+	{ "bash.sh", "*.sh" },
 	{ "main.c" },
-	"0r ~/library/skeletons/main.c",
-	augroup)
-
--- When creating a new c-main
-autocmd("BufNewFile",
 	{ "main.cpp" },
-	"0r ~/library/skeletons/main.cpp",
-	augroup)
-
--- When creating a main.js file
-autocmd("BufNewFile",
 	{ "main.js" },
-	"0r ~/library/skeletons/main.js",
-	augroup)
-
--- When creating an index-file
-autocmd("BufNewFile",
 	{ "index.html" },
-	"0r ~/library/skeletons/index.html",
-	augroup)
+	{ "build.xml" },
+	{ "main.py" },
+	{ ".classpath" },
+	{ ".project" },
+};
+
+for _, item in pairs(list) do
+
+	autocmd("BufNewFile",
+		item,
+		"0r ~/library/skeletons/"..item[1],
+		augroup)
+end
