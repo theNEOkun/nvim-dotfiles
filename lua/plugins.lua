640fe459ca5b52b6da9cd0943d7111a4d922ac80
theNEOkun <sven@raswill.se>
2022-11-30 14:06:24 +0100

    Moved around some more

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,6 +10,7 @@ require("dep") {
     'treesitter-conf',
     'todo-conf',
     'colors',
+    'rest',
   },
 
   --  'kosayoda/nvim-lightbulb'
@@ -44,46 +45,6 @@ require("dep") {
     'p00f/clangd_extensions.nvim'
   },
   -- JS
-  {
-    'rest-nvim/rest.nvim',
-    function() require('rest-nvim').setup({
-        -- Open request results in a horizontal split
-        result_split_horizontal = false,
-        -- Keep the http file buffer above|left when split horizontal|vertical
-        result_split_in_place = false,
-        -- Skip SSL verification, ful for unknown certificates
-        skip_ssl_verification = false,
-        -- Encode URL before making request
-        encode_url = true,
-        -- Highlight request on run
-        highlight = {
-          enabled = true,
-          timeout = 150,
-        },
-        result = {
-          -- toggle showing URL, HTTP info, headers at top the of result window
-          show_url = true,
-          show_http_info = true,
-          show_headers = true,
-          -- executables or functions for formatting response body [optional]
-          -- set them to nil if you want to disable them
-          formatters = {
-            json = "jq",
-            html = function(body)
-              return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-            end
-          },
-        },
-        -- Jump to request line on run
-        jump_to_request = false,
-        env_file = '.env',
-        custom_dynamic_variables = {},
-        yank_dry_run = true,
-      })
-    end,
-    opt = true,
-    ft = { 'http' },
-  },
 
   {
     'preservim/tagbar'
