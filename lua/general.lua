6456835d209719dff6fbc8ed4957a4414c7e56ba
theNEOkun <sven@raswill.se>
2022-12-13 10:52:50 +0100

    removed the mouse, and added some ;s

--- lua/general.lua
+++ lua/general.lua
@@ -1,39 +1,41 @@
 local g = vim.g;
-local set = vim.opt
+local set = vim.opt;
 
 local unpack = table.unpack or unpack;
 
--- Set the leader-key to spacebar
-require('utils').map('n', '<space>', '<nop>')
+-- Set the leader-key to spacebar;
+require('utils').map('n', '<space>', '<nop>');
 g.mapleader = " ";
 
--- Use relative linenumbers
-set.number = true
+-- Use relative linenumbers;
+set.number = true;
 set.relativenumber = true;
 set.signcolumn = "yes";
 
--- Use filetypes-plugin
+-- Use filetypes-plugin;
 set.filetype = "on";
 
--- Use tabstop
-set.tabstop = 2
-set.softtabstop = 2
-set.shiftwidth = 2
-set.expandtab = true
-set.smartindent = true
-set.wrap = false
-set.termguicolors = true
-set.timeoutlen = 500
-set.background = "dark"
+-- Use tabstop;
+set.tabstop = 2;
+set.softtabstop = 2;
+set.shiftwidth = 2;
+set.expandtab = true;
+set.smartindent = true;
+set.wrap = false;
+set.termguicolors = true;
+set.timeoutlen = 500;
+set.background = "dark";
 
-set.colorcolumn = "101"
+set.mouse = "";
 
-set.clipboard:prepend { "unnamed" }
+set.colorcolumn = "101";
 
-g.wildmenu = true
-g.wildmode = { "list", "longest", "full" }
-g.wildoptions = { "fuzzy" }
-vim.cmd(":set wildignore+=*/node_modules/*,*.obj,*.class,*/obj/*,tags,*/CMakeFiles/*,**/*.dex,**/build/*")
+set.clipboard:prepend { "unnamed" };
+
+g.wildmenu = true;
+g.wildmode = { "list", "longest", "full" };
+g.wildoptions = { "fuzzy" };
+vim.cmd(":set wildignore+=*/node_modules/*,*.obj,*.class,*/obj/*,tags,*/CMakeFiles/*,**/*.dex,**/build/*");
 
 -- set.path:append { "**" };
 
