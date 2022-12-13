7383fc424d87847b4dd3e5644103116f3afcfa0e
theNEOkun <sven@raswill.se>
2022-12-13 13:35:35 +0100

    Working on the keybinds

--- lua/packages/todo-conf.lua
+++ lua/packages/todo-conf.lua
@@ -13,13 +13,17 @@ return {
         return todo.setup(extra)
       end
 
-      keymap("n", "]t", function()
+      local start = '<leader>t';
+
+      keymap("n", start .. "n", function()
         todo.jump_next();
       end)
 
-      keymap("n", "[t", function()
+      keymap("n", start .. "p", function()
         todo.jump_prev();
       end)
+
+      keymap({ 'n' }, 'tt', '<cmd>:TodoQuickFix<cr>');
 
       return m;
 
--- lua/plugin-keymaps.lua
+++ lua/plugin-keymaps.lua
@@ -1,19 +1,21 @@
 local keymap = require('utils').map
 local cmd = require('utils').u_cmd;
 
+local tstart = '<leader>f';
 --Telescope
-keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
-keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
+keymap('n', tstart .. 'f', '<cmd>Telescope find_files<cr>')
+keymap('n', tstart .. 'g', '<cmd>Telescope live_grep<cr>')
+keymap('n', tstart .. 'b', '<cmd>Telescope buffers<cr>')
+keymap('n', tstart .. 'h', '<cmd>Telescope help_tags<cr>')
 
 -- keymap('n', '<F10>a', '<cmd>:TestSuite<CR>');
 -- keymap('n', '<F10>f', '<CMD>:TestFile<CR>');
 -- keymap('n', '<F10>s', '<CMD>:TestNearest<CR>');
 
-keymap('n', '<F10>a', ':lua require("neotest").run.run({suite = true})<CR>');
-keymap('n', '<F10>f', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>');
-keymap('n', '<F10>s', ':lua require("neotest").run.run()<CR>');
+-- local teststart = '<leader>q';
+-- keymap('n', teststart .. 'a', ':lua require("neotest").run.run({suite = true})<CR>');
+-- keymap('n', teststart .. 'f', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>');
+-- keymap('n', teststart .. 's', ':lua require("neotest").run.run()<CR>');
 
 -- REST keymaps
 keymap('n', '<leader>rt', '<Plug>RestNvim');
@@ -21,5 +23,3 @@ keymap('n', '<leader>rp', '<Plug>RestNvimPreview');
 
 -- Command to create ctags
 cmd('MakeTags', ':!ctags -R --exclude=node_modules --exclude="target/*" .');
-
-keymap({ 'n' }, 'tt', '<cmd>:TodoQuickFix<cr>');
