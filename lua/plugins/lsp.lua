dea3d7e5748e53f1b2bdb96e0c1306ff089ea8c1
theNEOkun <sven@raswill.se>
2022-12-21 10:28:36 +0100

    Did some restructuring

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,72 +1,72 @@
 local M = {
-    'VonHeikemen/lsp-zero.nvim',
-    dependencies = {
-        -- LSP Support
-        { 'neovim/nvim-lspconfig' },
-        { 'williamboman/mason.nvim' },
-        { 'williamboman/mason-lspconfig.nvim' },
+  'VonHeikemen/lsp-zero.nvim',
+  dependencies = {
+    -- LSP Support
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
 
-        -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },
-        { 'hrsh7th/cmp-buffer' },
-        { 'hrsh7th/cmp-path' },
-        { 'saadparwaiz1/cmp_luasnip' },
-        { 'hrsh7th/cmp-nvim-lsp' },
-        { 'hrsh7th/cmp-nvim-lua' },
+    -- Autocompletion
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-nvim-lua' },
 
-        -- Snippets
-        { 'L3MON4D3/LuaSnip' },
-        { 'rafamadriz/friendly-snippets' },
-        { 'nvim-telescope/telescope.nvim' }
-    },
-    config = function()
-        local lsp = require('lsp-zero');
-        lsp.preset('recommended');
+    -- Snippets
+    { 'L3MON4D3/LuaSnip' },
+    { 'rafamadriz/friendly-snippets' },
+    { 'nvim-telescope/telescope.nvim' }
+  },
+  config = function()
+    local lsp = require('lsp-zero');
+    lsp.preset('recommended');
 
-        lsp.ensure_installed({
-            'tsserver',
-            'sumneko_lua',
-            'clangd',
-            'jdtls',
-            'rust_analyzer'
-        });
+    lsp.ensure_installed({
+      'tsserver',
+      'sumneko_lua',
+      'clangd',
+      'jdtls',
+      'rust_analyzer'
+    });
 
-        local cmp = require('cmp');
-        local cmp_select = { behavior = cmp.SelectBehavior.Select };
-        local cmp_mappings = lsp.defaults.cmp_mappings({
-            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            ["<C-Space>"] = cmp.mapping.complete(),
-        });
+    local cmp = require('cmp');
+    local cmp_select = { behavior = cmp.SelectBehavior.Select };
+    local cmp_mappings = lsp.defaults.cmp_mappings({
+      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ["<C-Space>"] = cmp.mapping.complete(),
+    });
 
 
-        lsp.setup_nvim_cmp({
-            mapping = cmp_mappings
-        });
+    lsp.setup_nvim_cmp({
+      mapping = cmp_mappings
+    });
 
-        local list = require('plugins.lsp-conf.custom_servers')
+    local list = require('plugins.lsp-conf.custom_servers')
 
-        for client, opts in pairs(list) do
-            lsp.configure(client, opts.server_config());
-        end
+    for client, opts in pairs(list) do
+      lsp.configure(client, opts.server_config());
+    end
 
-        lsp.on_attach(function(client, bufnr)
-            if list[client] then
-                list[client].on_attach(client, bufnr);
-            else
-                require('plugins.lsp-conf.helper').on_attach(client, bufnr);
-            end
-        end)
+    lsp.on_attach(function(client, bufnr)
+      if list[client] then
+        list[client].on_attach(client, bufnr);
+      else
+        require('plugins.lsp-conf.helper').on_attach(client, bufnr);
+      end
+    end)
 
-        lsp.setup({
+    lsp.setup({
 
-        });
+    });
 
-        vim.diagnostic.config({
-            virtual_text = true,
-        })
-    end,
+    vim.diagnostic.config({
+      virtual_text = true,
+    })
+  end,
 }
 
 return { M };
