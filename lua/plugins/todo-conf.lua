3875373f2bef12736d8e691b0c47eda475cb6662
theNEOkun <sven@raswill.se>
2022-12-26 12:51:33 +0100

    Another small change

--- lua/plugins/todo-conf.lua
+++ lua/plugins/todo-conf.lua
@@ -1,32 +1,33 @@
-return {
-  {
-    'folke/todo-comments.nvim',
-    config = function()
+local M =
+{
+  'folke/todo-comments.nvim',
+  config = function()
 
-      local todo = require("todo-comments");
-      todo.setup();
-      local keymap = require("utils").map;
+    local todo = require("todo-comments");
+    todo.setup();
+    local keymap = require("utils").map;
 
-      local m = {};
+    local m = {};
 
-      m.setup = function(extra)
-        return todo.setup(extra)
-      end
-
-      local start = '<leader>f';
-
-      keymap("n", start .. "n", function()
-        todo.jump_next();
-      end, { desc = "[F]ix [N]ext" })
-
-      keymap("n", start .. "p", function()
-        todo.jump_prev();
-      end, { desc = "[F]ix [P]revious" })
-
-      keymap({ 'n' }, start .. 'l', '<cmd>:TodoQuickFix<cr>', { desc = "[F]ix [L]ist" });
-
-      return m;
-
+    m.setup = function(extra)
+      return todo.setup(extra)
     end
-  }
+
+    local start = '<leader>f';
+
+    keymap("n", start .. "n", function()
+      todo.jump_next();
+    end, { desc = "[F]ix [N]ext" })
+
+    keymap("n", start .. "p", function()
+      todo.jump_prev();
+    end, { desc = "[F]ix [P]revious" })
+
+    keymap({ 'n' }, start .. 'l', '<cmd>:TodoQuickFix<cr>', { desc = "[F]ix [L]ist" });
+
+    return m;
+
+  end
 }
+
+return { M }
