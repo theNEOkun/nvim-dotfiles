432dff6af954d480043bde5d65e3b14fd209acbc
theNEOkun <sven@raswill.se>
2022-12-30 12:07:24 +0100

    Added in some helpers for spelling

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,5 +1,4 @@
 local set = vim.opt;
-local keymap = require('utils').map;
 
 local basepath = vim.fn.stdpath('config') .. '/spell'
 set.spellfile = basepath .. '/en.utf-8.add' .. ', ' .. basepath .. '/sv.utf-8.add';
@@ -8,7 +7,3 @@ set.textwidth = 100;
 set.wrap = true;
 set.spell = true;
 set.spelllang:append({ 'en_gb', 'sv' });
-
-local dict = '<leader>w';
-keymap({ 'n', 'x' }, dict .. 'a', function() vim.cmd('normal! zg') end)
-keymap({ 'n', 'x' }, dict .. 'u', function() vim.cmd('normal! zuw') end)
