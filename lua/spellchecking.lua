0170181d9f12334ed0e6488a139baf8f887875f2
theNEOkun <sven@raswill.se>
2023-01-01 12:16:09 +0100

    Changed the keybinds for spellchecking some

--- lua/spellchecking.lua
+++ lua/spellchecking.lua
@@ -6,14 +6,14 @@ local basepath = vim.fn.stdpath('config') .. '/spell'
 set.spellfile = basepath .. '/en.utf-8.add' .. ', ' .. basepath .. '/sv.utf-8.add';
 set.spelllang:append({ 'en_gb', 'sv' });
 
-keymap({ 'n', 'x' }, dict .. 'a', function() vim.cmd('normal! zg') end, { desc = "[W]ord [A]dd" });
-keymap({ 'n', 'x' }, dict .. 'w', function() vim.cmd('normal! zuw') end, { desc = "[W]ord [W]rong add" });
-keymap({ 'n', 'x' }, dict .. 'ua', function() vim.cmd('normal! zg') end, { desc = "[W]ord [U]ndo [A]dd" });
-keymap({ 'n', 'x' }, dict .. 'uw', function() vim.cmd('normal! zuw') end, { desc = "[W]ord [U]ndo [W]rong" });
-keymap({ 'n' }, dict .. 'r', function() vim.cmd('normal! z=') end, { desc = "[W]ord [R]eplace" });
+keymap({ 'n', 'x' }, dict .. 'a', 'zg', { desc = "[W]ord [A]dd" });
+keymap({ 'n', 'x' }, dict .. 'w', 'zuw', { desc = "[W]ord [W]rong add" });
+keymap({ 'n', 'x' }, dict .. 'ua', 'zg', { desc = "[W]ord [U]ndo [A]dd" });
+keymap({ 'n', 'x' }, dict .. 'uw', 'zuw', { desc = "[W]ord [U]ndo [W]rong" });
+keymap({ 'n' }, dict .. 'r', 'z=', { desc = "[W]ord [R]eplace" });
 
-keymap({ 'n' }, dict .. 'n', function() vim.cmd('normal! ]s') end, { desc = "[W]ord [N]ext fault" });
-keymap({ 'n' }, dict .. 'p', function() vim.cmd('normal! [s') end, { desc = "[W]ord [P]revious fault" });
-keymap({ 'n' }, dict .. 'bn', function() vim.cmd('normal! ]S') end, { desc = "[W]ord [B]ad [N]ext" });
-keymap({ 'n' }, dict .. 'bp', function() vim.cmd('normal! [S') end, { desc = "[W]ord [B]ad [P]revous" });
+keymap({ 'n' }, dict .. 'n', ']s', { desc = "[W]ord [N]ext fault" });
+keymap({ 'n' }, dict .. 'p', '[s', { desc = "[W]ord [P]revious fault" });
+keymap({ 'n' }, dict .. 'bn', ']S', { desc = "[W]ord [B]ad [N]ext" });
+keymap({ 'n' }, dict .. 'bp', '[S', { desc = "[W]ord [B]ad [P]revous" });
 
