692853a37bd8b44e6e36e2bef97f3da9a4c641ac
theNEOkun <sven@raswill.se>
2023-01-06 11:05:53 +0100

    Some changes done

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,12 +5,23 @@ local cmd = require('utils').u_cmd;
 -- Command to create ctags
 cmd('MakeTags', ':!ctags -R --exclude=node_modules --exclude="target/*" .');
 
+local function get_visual_selection()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table.concat(lines, '\n')
+end
+
 keymap({ 'x' }, '<leader>b', ':!bc<CR>', { desc = "Calculate a chosen line using bc" });
 
 keymap({ 'n' }, '<F5>', ":put =strftime('%Y-%m-%dT%T%z')<cr>", { desc = "Enter todays date into the current buffer" });
-
--- require('utils').map('n', '<space>', '<nop>');
--- g.mapleader = " ";
 
 -- Edit vim
 keymap({ 'n' }, '<leader>ve', ':edit ~/.config/nvim/init.lua<cr>');
