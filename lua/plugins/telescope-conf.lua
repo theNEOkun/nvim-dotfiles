71cbf8757f6a92085f6479b14d8f81629b5f8f0b
theNEOkun <sven@raswill.se>
2023-01-09 11:53:28 +0100

    Changed the keymap, and used the builtin for telescope

--- lua/plugins/telescope-conf.lua
+++ lua/plugins/telescope-conf.lua
@@ -11,17 +11,25 @@ return {
       telescope.setup({
         defaults = {
           file_ignore_patterns = { "node_modules" },
+        },
+        pickers = {
+          find_files = {
+            theme = 'dropdown',
+          },
+          git_files = {
+            theme = 'dropdown',
+          }
         }
       });
       local builtin = require('telescope.builtin')
-      local tstart = '<leader>t';
+      local tstart = '<leader>f';
       --Telescope
-      keymap('n', tstart .. 'k', '<cmd>lua require("telescope.builtin").keymaps()<cr>', { desc = "[T]elescope [K]eymaps" })
+      keymap('n', tstart .. 'k', builtin.keymaps, { desc = "[T]elescope [K]eymaps" })
       keymap('n', tstart .. 'f', builtin.find_files, { desc = "[T]elescope (All) [T]elescope" })
       keymap('n', tstart .. 'g', builtin.git_files, { desc = "[T]elescope (Stored in) [G]it" })
-      keymap('n', tstart .. 's', '<cmd>Telescope live_grep<cr>', { desc = "[T]elescope [S]earch" })
-      keymap('n', tstart .. 'b', '<cmd>Telescope buffers<cr>', { desc = "[T]elescope [B]uffers"  })
-      keymap('n', tstart .. 'h', '<cmd>Telescope help_tags<cr>', { desc = "[T]elescope [H]elp-tags" })
+      keymap('n', tstart .. 's', builtin.live_grep, { desc = "[T]elescope [S]earch" })
+      keymap('n', tstart .. 'b', builtin.buffers, { desc = "[T]elescope [B]uffers"  })
+      keymap('n', tstart .. 'h', builtin.help_tags, { desc = "[T]elescope [H]elp-tags" })
     end
   }
 }
--- lua/plugins/which.lua
+++ lua/plugins/which.lua
@@ -3,7 +3,13 @@ return {
     'folke/which-key.nvim',
     config = function()
       local which = require('which-key');
-      which.setup({});
+      which.setup({
+        window = {
+          border = "single",
+          position = "bottom",
+          margin = { 0, 1, 0, 0 }
+        },
+      });
     end
   }
 }
