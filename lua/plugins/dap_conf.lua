5d5fc5270af32c8d02c0db96ba91aa93bfc4be38
theNEOkun <sven@raswill.se>
2023-01-09 11:15:43 +0100

    Moved around where files are

--- lua/plugins/dap_conf.lua
+++ lua/plugins/dap_conf.lua
@@ -1,88 +1,111 @@
-return {
-  {
-    'rcarriga/nvim-dap-ui',
-    dependencies = {
-      {
-        'mfussenegger/nvim-dap'
-      },
-      {
-        'jay-babu/mason-nvim-dap.nvim',
-        dependencies = {
-          'williamboman/mason.nvim',
-        }
-      },
-      {
-        'theHamsta/nvim-dap-virtual-text',
-        config = function()
-          M = require('nvim-dap-virtual-text').setup();
-          return M;
-        end,
-        dependencies = 'mfussenegger/nvim-dap'
-      },
+local function keymaps()
+  local keymap = require('utils').map
+
+  local start = '<leader>d';
+
+  keymap("n", start .. "ui", ":lua require('dapui').toggle()<CR>")
+  keymap("n", start .. "bt", ":lua require('dap').toggle_breakpoint()<CR>")
+  keymap("n", start .. "c", ":lua require('dap').continue()<CR>")
+
+  keymap("n", start .. "so", ":lua require('dap').step_over()<CR>")
+  keymap("n", start .. "si", ":lua require('dap').step_into()<CR>")
+  keymap("n", start .. "st", ":lua require('dap').step_out()<CR>")
+
+  keymap("n", start .. "hh", ":lua require('dap.ui.variables').hover()<CR>")
+  keymap("v", start .. "hv", ":lua require('dap.ui.variables').visual_hover()<CR>")
+
+  keymap("n", start .. "uh", ":lua require('dap.ui.widgets').hover()<CR>")
+  keymap("n", start .. "uf", ":lua local widgets=require('dap.ui.widgets');widgets.centered_float(widgets.scopes)<CR>")
+
+  keymap("n", start .. "sc", ":lua require('dap.ui.variables').scopes()<CR>")
+end
+
+local m = {
+  'rcarriga/nvim-dap-ui',
+  dependencies = {
+    {
+      'mfussenegger/nvim-dap'
     },
-    config = function()
-      local dap = require('dap')
-      local nvim_dap = require('mason-nvim-dap');
-      nvim_dap.setup({
-        automatic_setup = true,
-      });
-      nvim_dap.setup_handlers({
-        function(source_name)
-          -- all sources with no handler get passed here
-          -- Keep original functionality of `automatic_setup = true`
-          require('mason-nvim-dap.automatic_setup')(source_name)
-        end,
-        codelldb = function(source_name)
-          require('mason-nvim-dap.automatic_setup')(source_name)
-          dap.configurations.cpp = {
-            {
-              name = 'Launch debug',
-              type = 'codelldb',
-              request = 'launch',
-              program = function()
-                return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/bin/', 'file')
-              end,
-              cwd = '${workspaceFolder}',
-              stopOnEntry = false,
-              args = {},
-            }
+    {
+      'jay-babu/mason-nvim-dap.nvim',
+      dependencies = {
+        'williamboman/mason.nvim',
+      }
+    },
+    {
+      'theHamsta/nvim-dap-virtual-text',
+      config = function()
+        M = require('nvim-dap-virtual-text').setup();
+        return M;
+      end,
+      dependencies = 'mfussenegger/nvim-dap'
+    },
+  },
+  config = function()
+    local dap = require('dap')
+    local nvim_dap = require('mason-nvim-dap');
+    nvim_dap.setup({
+      automatic_setup = true,
+    });
+    nvim_dap.setup_handlers({
+      function(source_name)
+        -- all sources with no handler get passed here
+        -- Keep original functionality of `automatic_setup = true`
+        require('mason-nvim-dap.automatic_setup')(source_name)
+      end,
+      codelldb = function(source_name)
+        require('mason-nvim-dap.automatic_setup')(source_name)
+        dap.configurations.cpp = {
+          {
+            name = 'Launch debug',
+            type = 'codelldb',
+            request = 'launch',
+            program = function()
+              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/bin/', 'file')
+            end,
+            cwd = '${workspaceFolder}',
+            stopOnEntry = false,
+            args = {},
           }
-        end
-      });
-      require('plugins.dap.keymaps')
-      require('plugins.dap.adapters.lua_adapt')
-      --
-      -- local get_config_codelldb = function(path)
-      --   return {
-      --     name = "Launch file",
-      --     type = "codelldb",
-      --     request = "launch",
-      --     program = path,
-      --     cwd = vim.fn.getcwd(),
-      --     stopOnEntry = true,
-      --   }
-      -- end
-      --
-      -- dap.adapters.codelldb = {
-      --   type = 'server',
-      --   port = '${port}',
-      --   executable = {
-      --     command = 'codelldb',
-      --     args = { '--port', '${port}' },
-      --   }
-      -- }
-      --
-      -- dap.configurations.cpp = {
-      --   get_config_codelldb(function()
-      --     return vim.fn.input('Executable name: ', vim.fn.getcwd() .. '/bin/', 'file')
-      --   end)
-      -- }
-      --
-      -- dap.configurations.c = dap.configurations.cpp
-      --
-      -- dap.configurations.rust = { get_config_codelldb(function() return vim.fn.input('Executable name: ',
-      --     vim.fn.getcwd() .. '/target/debug/', 'file')
-      -- end), }
-    end
-  }
-};
+        }
+      end
+    });
+    keymaps();
+    require('plugins.dap.lua_adapt')
+
+    -- local get_config_codelldb = function(path)
+    --   return {
+    --     name = "Launch file",
+    --     type = "codelldb",
+    --     request = "launch",
+    --     program = path,
+    --     cwd = vim.fn.getcwd(),
+    --     stopOnEntry = true,
+    --   }
+    -- end
+    --
+    -- dap.adapters.codelldb = {
+    --   type = 'server',
+    --   port = '${port}',
+    --   executable = {
+    --     command = 'codelldb',
+    --     args = { '--port', '${port}' },
+    --   }
+    -- }
+    --
+    -- dap.configurations.cpp = {
+    --   get_config_codelldb(function()
+    --     return vim.fn.input('Executable name: ', vim.fn.getcwd() .. '/bin/', 'file')
+    --   end)
+    -- }
+    --
+    -- dap.configurations.c = dap.configurations.cpp
+    --
+    -- dap.configurations.rust = { get_config_codelldb(function() return vim.fn.input('Executable name: ',
+    --     vim.fn.getcwd() .. '/target/debug/', 'file')
+    -- end), }
+
+  end
+}
+
+return { m }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,54 @@
+-- Function that handles the creation of keymaps
+-- @return an object with an on_attach
+local function keymaps()
+  local M = {}
+
+  local utils = require('utils');
+  local keymap = utils.map
+  local buf_keymap = utils.buf_map
+
+  local start = '<leader>c'
+
+  -- Mappings.
+  -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+  keymap('n', start .. 'qf', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = "[C]ode [Q]uick [F]loat" });
+  keymap('n', start .. 'qp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { desc = "[C]ode [Q]uick [P]revious" });
+  keymap('n', start .. 'qn', '<cmd>lua vim.diagnostic.goto_next()<CR>', { desc = "[C]ode [Q]uick [N]ext" });
+  keymap('n', start .. 'ql', '<cmd>lua vim.diagnostic.setloclist()<CR>', { desc = "[C]ode [Q]uick [L]oc_list" });
+
+  -- Use an on_attach function to only map the following keys
+  -- after the language server attaches to the current buffer
+  M.on_attach = function(client, bufnr)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    buf_keymap(bufnr, 'n', start .. 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { desc = "[C]ode Open the hover-menu" });
+    buf_keymap(bufnr, 'n', start .. 's', require('telescope.builtin').lsp_references,
+      { desc = "[C]ode [S]how references" });
+    buf_keymap(bufnr, 'n', start .. 'a', '<cmd>lua vim.lsp.buf.code_action()<CR>',
+      { desc = "Selects a [C]ode [A]ction available" });
+    buf_keymap(bufnr, 'n', start .. 'f', '<cmd>lua vim.lsp.buf.format { async = true }<CR>', { desc = "[C]ode [F]ormat" });
+
+    buf_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { desc = "[G]o to Code [I]mplementation" });
+    buf_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { desc = "[G]o to Code [R]eferences" });
+    buf_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.type_definition()<CR>',
+      { desc = "[G]o to Code [Definition]" });
+    buf_keymap(bufnr, 'n', 'gVd', ':vsp | lua vim.lsp.buf.type_definition()<cr>',
+      { desc = "[G]o to Code [D]efinition in [V]ertical split" });
+    buf_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>',
+      { desc = "[G]o to Code Type [D]efinition" });
+
+    --Open context-menu
+    if client.name == "rust_analyzer" then
+      buf_keymap(bufnr, 'n', '<C-s>', '<cmd>:RustHoverActions<cr>', { desc = "Opens the Hover-Actions" });
+    else
+      buf_keymap(bufnr, 'n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
+        { desc = "Opens the hover-action menu" });
+    end
+  end
+
+  return M
+end
+
 local M = {
   'VonHeikemen/lsp-zero.nvim',
   dependencies = {
@@ -58,7 +109,7 @@ local M = {
       mapping = cmp_mappings
     });
 
-    local list = require('plugins.lsp-conf.custom_servers')
+    local list = require('plugins.lsp-conf')
 
     for client, opts in pairs(list) do
       lsp.configure(client, opts.server_config());
@@ -68,17 +119,18 @@ local M = {
       if list[client.name] then
         list[client.name].on_attach(client, bufnr);
       else
-        require('plugins.lsp-conf.helper').on_attach(client, bufnr);
+        keymaps().on_attach(client, bufnr);
       end
     end)
 
+    -- Add in null-ls as well there
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.prettier,
       },
-      on_attach = require('plugin.lsp-conf.helper').on_attach
+      on_attach = keymaps().on_attach
     });
 
     lsp.setup({
