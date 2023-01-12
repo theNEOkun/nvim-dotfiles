4765e56270a051b866667574ec0e02569f8fbc6b
theNEOkun <sven@raswill.se>
2023-01-12 22:04:07 +0100

    Moved ltex where it should be

--- lua/plugins/lsp-conf/init.lua
+++ lua/plugins/lsp-conf/init.lua
@@ -1,56 +1,3 @@
--- Function that handles the creation of keymaps
--- @return an object with an on_attach
-local function keymaps()
-  local M = {}
-
-  local utils = require('utils');
-  local keymap = utils.map
-  local buf_keymap = utils.buf_map
-
-  local start = '<leader>c'
-
-  -- Mappings.
-  -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-  keymap('n', start .. 'qf', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = "[C]ode [Q]uick [F]loat" });
-  keymap('n', start .. 'qp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { desc = "[C]ode [Q]uick [P]revious" });
-  keymap('n', start .. 'qn', '<cmd>lua vim.diagnostic.goto_next()<CR>', { desc = "[C]ode [Q]uick [N]ext" });
-  keymap('n', start .. 'ql', '<cmd>lua vim.diagnostic.setloclist()<CR>', { desc = "[C]ode [Q]uick [L]oc_list" });
-
-  -- Use an on_attach function to only map the following keys
-  -- after the language server attaches to the current buffer
-  M.on_attach = function(client, bufnr)
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_keymap(bufnr, 'n', start .. 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { desc = "[C]ode Open the hover-menu" });
-    buf_keymap(bufnr, 'n', start .. 's', require('telescope.builtin').lsp_references,
-      { desc = "[C]ode [S]how references" });
-    buf_keymap(bufnr, 'n', start .. 'a', '<cmd>lua vim.lsp.buf.code_action()<CR>',
-      { desc = "Selects a [C]ode [A]ction available" });
-
-    buf_keymap(bufnr, 'n', 'gI', '<cmd>lua vim.lsp.buf.implementation()<CR>', { desc = "[G]o to Code [I]mplementation" });
-    buf_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { desc = "[G]o to Code [R]eferences" });
-    buf_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.type_definition()<CR>',
-      { desc = "[G]o to Code [Definition]" });
-    buf_keymap(bufnr, 'n', 'gVd', ':vsp | lua vim.lsp.buf.type_definition()<cr>',
-      { desc = "[G]o to Code [D]efinition in [V]ertical split" });
-    buf_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>',
-      { desc = "[G]o to Code Type [D]efinition" });
-    buf_keymap(bufnr, 'n', start .. 'f', '<cmd>lua vim.lsp.buf.format({ async = true }) <CR>',
-      { desc = "[C]ode [F]ormat" });
-    buf_keymap(bufnr, 'x', start .. 'f', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>',
-      { desc = "[C]ode [F]ormat" });
-
-    --Open context-menu
-    if client.name == "rust_analyzer" then
-      buf_keymap(bufnr, 'n', start .. 'h', '<cmd>:RustHoverActions<cr>', { desc = "[C]ode [H]over-action" });
-    else
-      buf_keymap(bufnr, 'n', start .. 'h', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
-        { desc = "[C]ode [H]over-action" });
-    end
-  end
-
-  return M
-end
 
 local M = {
   'VonHeikemen/lsp-zero.nvim',
@@ -143,7 +90,7 @@ local M = {
       if list[client.name] then
         list[client.name].on_attach(client, bufnr);
       else
-        keymaps().on_attach(client, bufnr);
+        require( 'plugins.lsp-conf.helper' ).on_attach(client, bufnr);
       end
     end)
 
@@ -153,7 +100,7 @@ local M = {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.prettier,
       },
-      on_attach = keymaps().on_attach
+      on_attach = require( 'plugins.lsp-conf.helper' ).on_attach
     });
 
     lsp.setup();
