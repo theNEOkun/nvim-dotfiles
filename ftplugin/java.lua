6de7c5d7e3e72890de56cfc5da7826eaecbf13df
theNEOkun <sven@raswill.se>
2023-01-23 19:37:30 +0100

    Removed som smaller stuffs

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,7 +1,6 @@
 local keymap = require('utils').map;
 local u_cmd = require('utils').u_cmd;
 
-
 vim.cmd("compiler ant");
 local maven = "mvn";
 vim.cmd("set makeprg=" .. maven);
@@ -35,37 +34,3 @@ local constructor = '  public ' .. getName() .. "() { \t}"
 local lastLine = "}";
 
 u_cmd('Mf', ':norm i' .. pkgLine .. '<CR>' .. firstLine .. '<CR>' .. constructor .. '<CR>' .. lastLine)
-
-
--- NVIM-JDTLS
--- local jdtls = require('jdtls');
---
--- local home = os.getenv('HOME');
--- local root_dir = vim.fs.dirname(vim.fs.find({ '.gradlew', '.git', 'mvnw', 'pom.xml', '.classpath' }, { upward = true })[1]);
--- local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
---
--- local config = require 'coq'.lsp_ensure_capabilities {
---   cmd = {
---     home .. '/.local/share/nvim/mason/bin/jdtls'
---   },
---   root_dir = vim.fs.dirname(vim.fs.find({ '.gradlew', '.git', 'mvnw', 'pom.xml', '.classpath' }, { upward = true })[1]),
---   settings = {
---     java = {
---       configuration = {
---         -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
---         -- And search for `interface RuntimeOption`
---         -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
---         runtimes = {
---           {
---             name = "JavaSE-17",
---             path = "/usr/lib/jvm/java-17-openjdk/",
---           },
---         }
---       }
---     }
---   },
---   on_attach = function(client, bufnr)
---     require'jdtls.setup'.add_command();
---   end
--- }
--- jdtls.start_or_attach(config)
