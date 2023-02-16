980349f86017104d63b33fa8d1f58cf9213f1e28
theNEOkun <sven@raswill.se>
2023-02-16 12:57:31 +0100

    Fixed some sudden bugs

--- lua/plugins/lsp-conf/init.lua
+++ lua/plugins/lsp-conf/init.lua
@@ -40,7 +40,7 @@ local M = {
 
     -- This is installed on the computer already so...
     lsp.setup_servers({
-      'rust_analyzer',
+      -- 'rust_analyzer',
       -- 'hls',
       -- 'clangd',
       force = true,
