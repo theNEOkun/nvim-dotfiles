8fec477e5082f5736c40e9b522c83c3d4a6359e5
theNEOkun <sven@raswill.se>
2023-02-20 11:12:03 +0100

    Trying out oil.nvim

--- lua/plugins/netrw.lua
+++ lua/plugins/netrw.lua
@@ -1,9 +1,52 @@
 local g = vim.g;
-local M = {}
+local M = {
+  'stevearc/oil.nvim',
+  config = function()
+    local oil = require 'oil';
 
-g.netrw_banner = 0;
+
+    local command = "lua require('oil').open()"
+
+    local cmd = require('utils').u_cmd;
+    -- Just explore
+    cmd('Ex', function() vim.cmd(command) end)
+    -- Horisontal explore
+    cmd('Sex', function()
+      vim.cmd('split')
+      vim.cmd(command)
+    end)
+    -- Vertical Explore
+    cmd('Vex', function()
+      vim.cmd('vsplit')
+      vim.cmd(command)
+    end)
+
+
+    local options = {
+      columns = {
+        "icon",
+        -- "permissions",
+        -- "size",
+        -- "mtime",
+      },
+      win_options = {
+        signcolumn = "yes",
+      },
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = false,
+      },
+    }
+
+    g.loaded_netrw = 1
+    g.loaded_netrwPlugin = 1
+    oil.setup(options);
+  end
+}
+
+-- g.netrw_banner = 0;
+-- g.netrw_hide = 0;
 -- g.netrw_liststyle = 3;
-g.netrw_hide = 0;
 
 -- local m = {
 --     'prichrd/netrw.nvim',
