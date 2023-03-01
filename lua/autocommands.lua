1c1520be1959020656e4e695237914cae7490d73
theNEOkun <sven@raswill.se>
2023-03-01 13:33:01 +0100

    Updated some

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -3,3 +3,7 @@ local autocmd = require('utils').autocmd;
 
 autocmd('InsertEnter', '*', ':set norelativenumber')
 autocmd('InsertLeave', '*', ':set relativenumber')
+
+-- Try to discover ron-files
+local ft = require('utils').autogroup("filetypes");
+require('utils').autocmd({'BufRead', 'BufNewFile'}, '*.ron', 'set filetype=ron', ft);
