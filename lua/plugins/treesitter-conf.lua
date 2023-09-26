1c09dcae3eb8afb9fb6159cf1c4e3707a857f98a
theNEOkun <sven@raswill.se>
2023-09-26 20:35:44 +0200

    Some changes

--- lua/plugins/treesitter-conf.lua
+++ lua/plugins/treesitter-conf.lua
@@ -5,7 +5,7 @@ return {
       { 'windwp/nvim-ts-autotag' },
       { 'mrjones2014/nvim-ts-rainbow' },
       { 'RRethy/nvim-treesitter-endwise' },
-      { 'nvim-treesitter/nvim-treesitter-textobjects' },
+      -- { 'nvim-treesitter/nvim-treesitter-textobjects' },
       { 'JoosepAlviste/nvim-ts-context-commentstring' },
       { 'nvim-treesitter/playground' },
     },
@@ -18,8 +18,7 @@ return {
         -- Install languages synchronously (only applied to `ensure_installed`)
         sync_install = false,
         highlight = {
-          enable = true,
-          additional_vim_regex_highlightning = false,
+          enable = true
         },
         incremental_selection = {
           enable = true,
@@ -46,50 +45,6 @@ return {
         },
         autotag = {
           enable = true
-        },
-        textobjects = {
-          select = {
-            enable = true,
-
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
-
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ai"] = "@conditional.outer",
-              ["ii"] = "@conditional.inner",
-              -- ["al"] = "@loop.outer",
-              -- ["il"] = "@loop.inner",
-              ["ac"] = "@class.outer",
-              -- You can optionally set descriptions to the mappings (used in the desc parameter of
-              -- nvim_buf_set_keymap) which plugins like which-key display
-              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-            },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
-            selection_modes = {
-              ['@parameter.outer'] = 'v', -- charwise
-              ['@function.outer'] = 'V', -- linewise
-              ['@class.outer'] = '<c-v>', -- blockwise
-            },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true of false
-            include_surrounding_whitespace = false,
-          },
         },
       }
     end
